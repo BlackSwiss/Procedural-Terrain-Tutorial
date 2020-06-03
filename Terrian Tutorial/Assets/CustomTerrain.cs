d54db09d34fd6a815c7fabab9d125fdd8730ceeb
BlackSwiss <dylan.ayala562@gmail.com>
2020-06-02 18:12:32 -0700

    set up skeleton for terrain

---
+++ Terrian Tutorial/Assets/CustomTerrain.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Want this code to run in edit not when we press play
+[ExecuteInEditMode]
+
+
+public class CustomTerrain : MonoBehaviour {
+
+    void Awake()
+    {
+        //Will be used to create custom tags and automatically assign those tags onto objects
+
+        //Serialized allow to sync values between editor and code
+
+        //Tag manager given path to pick up default tags and other tags made
+        SerializedObject tagManager = new SerializedObject(
+                                                            AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        //Apply tag changes to tag database
+        tagManager.ApplyModifiedProperties();
+
+        //Take this object
+        this.gameObject.tag = "Terrain";
+    }
+
+    void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+
+        //ensure the tag doesn't already exist
+        for(int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            //if tag exists, set found to true
+            if (t.stringValue.Equals(newTag)) { found = true; break; }
+        }
+
+        //add your new tag if tag does not exist
+        if (!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Terrian Tutorial/Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+//Links the 2 scripts together
+[CustomEditor(typeof(CustomTerrain))]
+
+//Multiple objects will be edited
+[CanEditMultipleObjects]
+
+public class CustomTerrainEditor : Editor {
+
+    //everytime we add something new in editor, terrain will renable and rerun initialization
+    //Dont need to press play everytime to see changes
+    void OnEnable()
+    {
+        
+    }
+
+    //Graphical user interface we will see in inspector for custom terrain editor
+    //Will get long
+    public override void OnInspectorGUI()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
